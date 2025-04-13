c4a50f6fab263ebbfa40c2d5be87ff94ace1481e
azizdev <abdukarimovtheaziz@gmail.com>
2025-04-13 18:04:08 +0500

    Initial commit

---
+++ src/test/java/dev/aziz/echobot/EchobotApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.aziz.echobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EchobotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
